46b11f76da2036d0df7b6435801c5e50407b069c
Florian Gareis <flo.gareis@tum.de>
2018-11-21 16:28:18 +0100

    #8 - Fix files according to tslint

--- tooling/src/index.ts
+++ tooling/src/index.ts
@@ -2,7 +2,7 @@ import * as simplegit from 'simple-git/promise';
 import * as jsonforms from '@jsonforms/core';
 import * as cp from 'child_process';
 import { writeFile, readFile } from 'fs';
-var npm = process.platform === 'win32' ? 'npm.cmd' : 'npm';
+const npm = process.platform === 'win32' ? 'npm.cmd' : 'npm';
 
 /*
  * Clones a git repository and runs npm install on it
@@ -11,27 +11,27 @@ var npm = process.platform === 'win32' ? 'npm.cmd' : 'npm';
  * @param {function} callback forwards the current status to the caller
  */
 export function cloneAndInstall(repo: String, path: string, callback: (result: string, type?: string) => void) {
-    var url = '';
-    switch(repo) {
-        case 'example':
-            url = 'https://github.com/eclipsesource/make-it-happen-react';
-            break;
-        case 'seed':
-            url = 'https://github.com/eclipsesource/jsonforms-react-seed';
-            break;
-    }
-    const git = simplegit();
-    callback('Starting to clone repo');
-    git.clone(url, path)
-        .then(function() {
-            callback('Finished to clone repo');
-            callback('Running npm install');
-            const result = cp.spawnSync( npm, ['install'], {
-                cwd: path
-            }); 
-            callback(result.signal); 
-        })
-        .catch((err: any) => {callback(err.message, 'err')});
+  let url = '';
+  switch (repo) {
+    case 'example':
+      url = 'https://github.com/eclipsesource/make-it-happen-react';
+      break;
+    case 'seed':
+      url = 'https://github.com/eclipsesource/jsonforms-react-seed';
+      break;
+  }
+  const git = simplegit();
+  callback('Starting to clone repo');
+  git.clone(url, path)
+    .then(() => {
+      callback('Finished to clone repo');
+      callback('Running npm install');
+      const result = cp.spawnSync(npm, ['install'], {
+        cwd: path,
+      });
+      callback(result.signal);
+    })
+    .catch((err: any) => { callback(err.message, 'err'); });
 }
 
 /**
@@ -40,18 +40,21 @@ export function cloneAndInstall(repo: String, path: string, callback: (result: s
  * @param {function} callback forwards the current status to the caller
  */
 export function generateUISchema(path: string, callback: (result: string, type?: string) => void) {
-    readFile(path, 'utf8', (err, data) => {
-        if (err) callback(err.message, 'err');
-        var jsonSchema = JSON.parse(data);
-        var jsonUISchema = jsonforms.generateDefaultUISchema(jsonSchema);
-        if(process.platform === 'win32') {
-            var newPath = path.substring(0, path.lastIndexOf("\\"))+'\\ui-schema.json';
-        } else {
-            var newPath = path.substring(0, path.lastIndexOf("/"))+'/ui-schema.json';
-        }
-        writeFile(newPath, JSON.stringify(jsonUISchema,null, 2), (err) => {
-            if (err) callback(err.message, 'err');
-            callback('Successfully generated UI schema');
-        });
+  readFile(path, 'utf8', (err, data) => {
+    if (err) callback(err.message, 'err');
+    const jsonSchema = JSON.parse(data);
+    const jsonUISchema = jsonforms.generateDefaultUISchema(jsonSchema);
+    let newPath = '';
+    if (process.platform === 'win32') {
+      newPath = path.substring(0, path.lastIndexOf('\\'));
+      newPath = `${newPath}\\ui-schema.json`;
+    } else {
+      newPath = path.substring(0, path.lastIndexOf('/'));
+      newPath = `${newPath}/ui-schema.json`;
+    }
+    writeFile(newPath, JSON.stringify(jsonUISchema, null, 2), (err) => {
+      if (err) callback(err.message, 'err');
+      callback('Successfully generated UI schema');
     });
+  });
 }
--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -2,57 +2,54 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-var tooling = require('jsonforms-tooling');
+const tooling = require('jsonforms-tooling');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let createExampleProject = vscode.commands.registerCommand('extension.createExampleProject', (args: any) => {
-        if(!args) {
-            showMessage('You can only run this on a folder', 'err');
-            return;
-        } else {
-            let path = args.fsPath;
-            showMessage('Creating example project: '+path);
-            tooling.cloneAndInstall('example', path, function(result: string, type: string) {
-                showMessage(result, type);
-            });
-        }
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  const createExampleProject = vscode.commands.registerCommand('extension.createExampleProject', (args: any) => {
+    if (!args) {
+      showMessage('You can only run this on a folder', 'err');
+      return;
+    }
+    const path = args.fsPath;
+    showMessage(`Creating example project: ${path}`);
+    tooling.cloneAndInstall('example', path, (result: string, type: string) =>  {
+      showMessage(result, type);
     });
+  });
 
-    let createSeedProject = vscode.commands.registerCommand('extension.createSeedProject', (args: any) => {
-        if(!args) {
-            showMessage('You can only run this on a folder', 'err');
-            return;
-        } else {
-            let path = args.fsPath;
-            showMessage('Creating seed project: '+path);
-            tooling.cloneAndInstall('seed', path, function(result: string, type: string) {
-                showMessage(result, type);
-            });
-        }
+  const createSeedProject = vscode.commands.registerCommand('extension.createSeedProject', (args: any) => {
+    if (!args) {
+      showMessage('You can only run this on a folder', 'err');
+      return;
+    }
+    const path = args.fsPath;
+    showMessage(`Creating seed project: ${path}`);
+    tooling.cloneAndInstall('seed', path, (result: string, type: string) => {
+      showMessage(result, type);
     });
+  });
 
-    let generateUISchema = vscode.commands.registerCommand('extension.generateUISchema', (args: any) => {
-        if(!args) {
-            showMessage('You can only run this on a json file', 'err');
-            return;
-        } else {
-            let path = args.fsPath;
-            showMessage('Generating UI Schema: '+path);
-            tooling.generateUISchema(path, function(result: string, type: string) {
-                showMessage(result, type);
-            });
-        }
+  const generateUISchema = vscode.commands.registerCommand('extension.generateUISchema', (args: any) => {
+    if (!args) {
+      showMessage('You can only run this on a json file', 'err');
+      return;
+    }
+    const path = args.fsPath;
+    showMessage(`Generating UI Schema: ${path}`);
+    tooling.generateUISchema(path, (result: string, type: string) => {
+      showMessage(result, type);
     });
+  });
 
-    context.subscriptions.push(createExampleProject);
-    context.subscriptions.push(createSeedProject);
-    context.subscriptions.push(generateUISchema);
+  context.subscriptions.push(createExampleProject);
+  context.subscriptions.push(createSeedProject);
+  context.subscriptions.push(generateUISchema);
 }
 
 /**
@@ -61,16 +58,16 @@ export function activate(context: vscode.ExtensionContext) {
  * @param {string} type the type of the message
  */
 function showMessage(message: string, type?: string) {
-    switch(type) {
-        case 'err':
-            vscode.window.showErrorMessage(message);
-            break;
-        case 'war':
-            vscode.window.showWarningMessage(message);
-            break;
-        default:
-            vscode.window.showInformationMessage(message);
-    }
+  switch (type) {
+    case 'err':
+      vscode.window.showErrorMessage(message);
+      break;
+    case 'war':
+      vscode.window.showWarningMessage(message);
+      break;
+    default:
+      vscode.window.showInformationMessage(message);
+  }
 }
 
 // this method is called when your extension is deactivated
