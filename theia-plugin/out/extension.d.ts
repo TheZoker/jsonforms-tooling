bf275cbe4456f6950371d69e442fcd874525f29f
Florian Gareis <flo.gareis@tum.de>
2018-11-21 17:19:44 +0100

    #8 - Move theia plugin output to out folder

---
+++ theia-plugin/out/extension.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Generated using theia-plugin-generator
+ */
+import * as theia from '@theia/plugin';
+export declare function start(context: theia.PluginContext): void;
+export declare function stop(): void;
